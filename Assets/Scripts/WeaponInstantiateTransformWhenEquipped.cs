9d73bd62e2664da1f668f568ad8cdc18329100cb
Tú buồn bã <nguyentu.tn.2002@gmail.com>
2025-03-31 12:29:31 +0700

    Organize.

--- Assets/Scripts/WeaponInstantiateTransformWhenEquipped.cs
+++ Assets/Scripts/WeaponInstantiateTransformWhenEquipped.cs
@@ -25,9 +25,46 @@ namespace NT
             if (weaponGameObject != null)
             {
                 weaponPrefabInstantiatedInThisHand = weaponGameObject;
-                weaponGameObject.transform.parent = transform;
-                weaponGameObject.transform.localPosition = Vector3.zero;
-                weaponGameObject.transform.localRotation = Quaternion.identity;
+
+                //  DEBUG FOR SPAWN GAMEOBJECTS MODELS
+                switch (weaponInstantiateSlot)
+                {
+                    case WeaponInstantiateSlot.MainHandSlot:
+                        weaponGameObject.transform.parent = transform;
+                        weaponGameObject.transform.localPosition = Vector3.zero;
+                        weaponGameObject.transform.localRotation = Quaternion.identity;
+                        break;
+                    case WeaponInstantiateSlot.OffHandSlot:
+                        weaponGameObject.transform.parent = transform;
+                        weaponGameObject.transform.localPosition = Vector3.zero;
+                        weaponGameObject.transform.localRotation = Quaternion.identity;
+                        break;
+                    case WeaponInstantiateSlot.OffHand_Shield_Slot:
+                        weaponGameObject.transform.parent = transform;
+                        weaponGameObject.transform.localPosition = Vector3.zero;
+                        weaponGameObject.transform.localRotation = Quaternion.identity;
+                        break;
+                    case WeaponInstantiateSlot.BackSlot:
+                        //  DEBUG RIGHT NOW FOR TWO HANDED STYLE
+                        if (weapon.weaponType != WeaponType.Shield_Weapon)
+                        {
+                            //  IF WEAPON IS NOT A SHIELD, JUST SPAWN NORMALY
+                            weaponGameObject.transform.parent = transform;
+                            weaponGameObject.transform.localPosition = Vector3.zero;
+                            weaponGameObject.transform.localRotation = Quaternion.identity;
+                        }
+                        //  OTHERWISE, IF IS SHIELD, SPAWN WITH THAT VALUES
+                        else
+                        {
+                            weaponGameObject.transform.parent = transform;
+                            weaponGameObject.transform.localPosition = new Vector3(0.212543f, -0.1514575f, -0.4658348f);
+                            weaponGameObject.transform.localRotation = Quaternion.Euler(-22.719f, 65.157f, 149.634f);
+                        }
+                        break;
+                    default:
+                        break;
+                }
+
                 weaponGameObject.transform.localScale = Vector3.one;
             }
         }
